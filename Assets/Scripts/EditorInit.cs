68b1f881a39d4829a68a7864e7a51e7f1603e3ee
Vlad K <CoruhS5@gmail.com>
2022-10-12 16:03:30 +0200

    [add] GameLevel

---
+++ Assets/Scripts/EditorInit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Logic
+{
+#if UNITY_EDITOR
+    [InitializeOnLoad]
+    public class EditorInit
+    {
+        static EditorInit()
+        {
+            var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
+            var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
+            EditorSceneManager.playModeStartScene = sceneAsset;
+            Debug.Log(pathOfFirstScene + " was set as default play mode scene");
+        }
+    }
+    #endif
+}
